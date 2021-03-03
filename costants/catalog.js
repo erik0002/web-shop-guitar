70da93443549dc997597d66b444d8863747e109c
Yeliazar <Erik.ruzhakov@mail.ru>
2021-03-03 18:13:03 +0300

    AddProducts
    Add products on the site

--- costants/catalog.js
+++ costants/catalog.js
@@ -15,12 +15,12 @@ const CATALOG = [
         id: 'el3',
         name: 'Электроакустическая гитара Yamaha CX-40',
         img: 'https://muz.by/upload/resize_cache/iblock/f9d/210_200_1/f9d336759456b6f41115e5fa66b2f985.jpg',
-        price: 0,
+        price: 1500,
     }, 
     {
         id: 'el4',
         name: 'ТрансАкустическая гитара Yamaha FG-TA BS',
         img: 'https://muz.by/upload/resize_cache/iblock/cd5/210_200_1/cd521b73a37c1d19c01b876679ca3716.jpg',
-        price: 0,
+        price: 560,
     }
 ];
