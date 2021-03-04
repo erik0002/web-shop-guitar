4d1699ddb95b208a8d04a49e4af50ceeaf291cd3
Yeliazar <Erik.ruzhakov@mail.ru>
2021-03-04 19:53:00 +0300

    localStorage
    Add with Local Storage

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,8 +1,24 @@
 class Products {
+    constructor(){
+        this.classNameActive = 'products-element__btn_active';
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRemove = 'Удалить из корзины';
+    }
     
     render(){
+        const productsStore = localStrorageUtil.getProducts();
         let htmlCatalog = ''; 
         CATALOG.forEach(({id, name, price, img}) => {
+                let activeClass = '';
+                let activeText = '';
+
+                if(productsStore.indexOf(id) === -1 ){
+                  activeText = this.labelAdd;
+                } else {
+                  activeClass = ' ' + this.classNameActive;
+                  activeText = this.labelRemove;
+                }
+
                 htmlCatalog += `
                   <li class="products-element">
                     <span class="products-element__name">${name}</span>
@@ -10,7 +26,7 @@ class Products {
                     <span class="products-element__price">  
                         🔥 ${price.toLocaleString()} USD
                     </span>
-                    <button class="products-element__btn"> Добавить в корзину </button>
+                    <button class="products-element__btn ${activeClass}"> ${activeText} </button>
                   </li>
                 `;
         });
